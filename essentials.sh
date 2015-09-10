73abbb80f97df043dae067a618716f6b6c9bcba5
zachw000 <wewingames222@gmail.com>
2015-09-10 16:58:48 -0700

    Added more functionality

--- essentials.sh
+++ essentials.sh
@@ -7,11 +7,20 @@ sudo apt-get install vim
 sudo apt-get install git
 clear
 while true; do
+	read -p "Do you want to install Spotify (r)?" yn
+	case $yn in
+		[Yy]* ) sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886; echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list; sudo apt-get update; sudo apt-get install spotify-client; break;;
+		[Nn]* ) break;;
+		* ) echo "Please answer yes or no [y/n].";
+	esac
+done
+clear
+while true; do
 	read -p "Do you want to install Java (tm)?" yn
 	case $yn in
 		[Yy]* ) sudo add-apt-repository ppa:webupd8team/java; sudo apt-get update; sudo apt-get install oracle-java8-installer; break;;
 		[Nn]* ) break;;
-		* ) echo "Please answer yes or no.";;
+		* ) echo "Please answer yes or no.";
 	esac
 done
 wget http://download.skype.com/linux/skype-ubuntu-precise_4.3.0.37-1_i386.deb
@@ -42,7 +51,7 @@ if [ -f ./google-chrome-stable_current_amd64.deb ]; then
 	echo "Installed Google Chrome!"
 fi
 # Remove File after install to save disk space
-rm skype-ubuntu-precise_4.30.0.37-1_i386.deb
+rm skype-ubuntu-precise_4.3.0.37-1_i386.deb
 rm google-chrome-stable_current_amd64.deb
 
 # Clear the screen to make sure all extra text is removed (Google Chrome WILL throw an error but that's ok)
